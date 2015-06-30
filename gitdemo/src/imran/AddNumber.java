11f709ef5bc3a9388baacea8f53287a15442f959
imran <home@home-HP>
2015-06-30 11:46:08 +0530

    this is my first

---
+++ gitdemo/src/imran/AddNumber.java
@@ -0,0 +1,15 @@
+package imran;
+
+public class AddNumber {
+	public static void main(String args[]){
+		int x,y;
+		x=10;
+		y=20;
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println(x+"x value"+y+" y value" );
+	}
+
+}
